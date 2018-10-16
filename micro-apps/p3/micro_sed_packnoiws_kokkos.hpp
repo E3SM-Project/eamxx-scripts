f1d59421ea5fef3818f2231a88f6bf54242a94d1
James Foucar <jgfouca@sandia.gov>
2018-10-15 21:17:03 -0600

    Minor fixes

--- micro-apps/p3/micro_sed_packnoiws_kokkos.hpp
+++ micro-apps/p3/micro_sed_packnoiws_kokkos.hpp
@@ -41,7 +41,7 @@ public:
     vn_table("VN_TABLE"), vm_table("VM_TABLE"),
     mu_r_table("MU_R_TABLE"),
     policy(util::ExeSpaceUtils<>::get_default_team_policy(num_horz, num_pack)),
-    workspace_mgr(num_pack, num_horz_, policy)
+    workspace_mgr(num_pack, 8, policy) // rain sed's high-water is 8 workspace for any team
   {
     // initialize on host
 
@@ -344,7 +344,7 @@ void micro_sed_func (
       }
     });
 
-  m.workspace_mgr.report();
+  // m.workspace_mgr.report(); uncomment for detailed debug info
 }
 
 } // namespace p3
