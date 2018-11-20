1377733e841971ef08e71fe824203d3387156d97
James Foucar <jgfouca@sandia.gov>
2018-11-20 11:05:56 -0700

    First stab at documenting WSM and p3_functions

--- micro-apps/p3/wsm.hpp
+++ micro-apps/p3/wsm.hpp
@@ -9,10 +9,46 @@ struct UnitWrap;
 
 namespace util {
 
+/*
+ * WorkspaceManager is a utility for requesting workspaces
+ * (temporary memory blocks) from within a Kokkos kernel. Workspaces
+ * are shared within thread teams.
+ *
+ * The WorkspaceManager should be initialized before Kokkos kernels
+ * are run.  Users need to specify the size of each individual
+ * sub-block (in terms of number of T's), the maximum number of
+ * sub-blocks (AKA "spaces") they intend to use, and the team policy
+ * that they intend to use for their Kokkos kernels.
+ *
+ * Once inside a Kokkos kernel, the user will call get_workspace on
+ * their WorkspaceManager. This will return a Workspace object which
+ * is the user's iterface to the entire memory block, which mostly
+ * involves taking/releasing memory sub-blocks (represented by kokkos
+ * views). The API for taking/releasing sub-blocks has a number of
+ * variants in order to offer both simplicity/clarity and high
+ * performance. The simplest way to use your Workspace is to manage
+ * your sub-blocks individually with the "take" and "release"
+ * methods. You'll get better performance with the methods that deal
+ * with the sub-blocks in bulk.  We've seen the best performance by
+ * making a single take_many_and_reset call at the beginning of the
+ * Kokkos kernel, though this may not be practical for large
+ * kernels. High-granularity take/release calls can allow you to use a
+ * smaller Workspace (by allowing you to use a smaller max_used).
+ *
+ * You can fine-tune the max_used by making a large overestimate for
+ * this value, running your kernel, and then calling report, which
+ * will tell you the actual maximum number of sub-blocks that you
+ * used. Note that all sub-blocks have a name.
+ */
+
 template <typename T, typename DeviceT=DefaultDevice>
 class WorkspaceManager
 {
  public:
+
+  //
+  // ---------- Types ---------
+  //
 
   using Device = DeviceT;
 
@@ -31,52 +67,75 @@ class WorkspaceManager
   using view_1d_ptr_array = typename KokkosTypes<Device>::template view_1d_ptr_array<S, N>;
 
   //
-  // public API
+  // ------- public API ---------
   //
 
+  // Constructor, call from host
+  //   size: The number of T's per sub-block
+  //   max_used: The maximum number of active sub-blocks
+  //   policy: The team policy for Kokkos kernels using this WorkspaceManager
   WorkspaceManager(int size, int max_used, TeamPolicy policy);
 
+  // call from host.
+  //
+  // Will report usage statistics for your workspaces. These statistics will
+  // have much more detail for debug builds.
   void report() const;
 
   class Workspace;
 
+  // call from device
+  //
+  // Returns a Workspace object which provides access to sub-blocks.
   KOKKOS_INLINE_FUNCTION
   Workspace get_workspace(const MemberType& team) const;
 
   class Workspace {
    public:
+
+    // Take an individual sub-block
     template <typename S=T>
     KOKKOS_INLINE_FUNCTION
     Unmanaged<view_1d<S> > take(const char* name) const;
 
-    template <size_t N, typename S=T>
-    KOKKOS_INLINE_FUNCTION
-    void take_many_contiguous_unsafe(const Kokkos::Array<const char*, N>& names,
-                                     const view_1d_ptr_array<S, N>& ptrs) const;
-
+    // Take several sub-blocks. The user gets pointers to their sub-blocks
+    // via the ptrs argument.
     template <size_t N, typename S=T>
     KOKKOS_INLINE_FUNCTION
     void take_many(const Kokkos::Array<const char*, N>& names,
                    const view_1d_ptr_array<S, N>& ptrs) const;
 
+    // Similar to take_many except assumes that there is enough contiguous
+    // memory avaiable in the Workspace for N sub-blocks. This method is higher-performing
+    // than take_many.
+    template <size_t N, typename S=T>
+    KOKKOS_INLINE_FUNCTION
+    void take_many_contiguous_unsafe(const Kokkos::Array<const char*, N>& names,
+                                     const view_1d_ptr_array<S, N>& ptrs) const;
+
+    // Combines reset and take_many_contiguous_unsafe. This is the most-performant
+    // option for a kernel to use N sub-blocks that are needed for the duration of the
+    // kernel.
     template <size_t N, typename S=T>
     KOKKOS_INLINE_FUNCTION
     void take_many_and_reset(const Kokkos::Array<const char*, N>& names,
                              const view_1d_ptr_array<S, N>& ptrs) const;
 
-    // Wrapper so caller doesn't have to specify scalar type.
+    // Release an individual sub-block.
     template <typename View>
     KOKKOS_FORCEINLINE_FUNCTION
     void release(const View& space, std::enable_if<View::rank == 1>* = 0) const
     { release_impl<typename View::value_type>(space); }
 
 #ifndef NDEBUG
+    // Get the name of a sub-block
     template <typename View>
     KOKKOS_INLINE_FUNCTION
     const char* get_name(const View& space, std::enable_if<View::rank == 1>* = 0) const
     { return get_name_impl<typename View::value_type>(space); }
 #endif
 
+    // Reset back to initial state. All sub-blocks will be considered inactive.
     KOKKOS_INLINE_FUNCTION
     void reset() const;
 
@@ -84,7 +143,7 @@ class WorkspaceManager
     void print() const;
 
     //
-    // Private
+    // ---------- Private --------------
     //
 
 #ifndef KOKKOS_ENABLE_CUDA
