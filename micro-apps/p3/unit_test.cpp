4d63999025c172bc3463878499bc4bf7c0232761
Andrew M. Bradley <ambrad@localhost>
2018-12-12 10:50:38 -0700

    Refactor for GPU.
    I forgot that Cuda doesn't accept lambda functions from outside of the kernel
    since they have local types.

--- micro-apps/p3/unit_test.cpp
+++ micro-apps/p3/unit_test.cpp
@@ -212,7 +212,8 @@ static int unittest_find_top_bottom(int max_threads)
 // the domains over which the tables are defined and outside them. The primary
 // tool is measuring the maximum slope magnitude as a function of mesh
 // refinement, where the mesh is a 1D mesh transecting the table domain.
-static int unittest_table3 (int max_threads) {
+
+struct TestTable3 {
   using Functions = p3::micro_sed::Functions<Real, Device>;
   using view_1d_table = typename Functions::view_1d_table;
   using view_2d_table = typename Functions::view_2d_table;
@@ -222,127 +223,128 @@ static int unittest_table3 (int max_threads) {
   using Table3 = typename Functions::Table3;
   using RangePolicy = Kokkos::RangePolicy<typename Device::execution_space>;
 
-  Int nerr = 0;
-
-  // This test doesn't use mu_r_table, as that is not a table3 type. It doesn't
-  // matter whether we use vm_table or vn_table, as the table values don't
-  // matter in what we are testing; we are testing interpolation procedures that
-  // are indepennt of particular table values.
-  view_1d_table mu_r_table;
-  view_2d_table vn_table, vm_table;
-  Functions::init_kokkos_tables(vn_table, vm_table, mu_r_table);
-
-  // Parameters for lower and upper bounds, derived above, multiplied by factors
-  // so we go outside of the bounds a bit.
-  const Scalar lamr_lo = 0.1*116.3, lamr_hi = 1.5*0.2e6;
-  // Using these, we map alpha in [0,1] -> meaningful lamr.
-  const auto calc_lamr = [&] (const Scalar& mu_r, const Scalar& alpha) {
+  KOKKOS_FUNCTION static Scalar calc_lamr (const Scalar& mu_r, const Scalar& alpha) {
+    // Parameters for lower and upper bounds, derived above, multiplied by
+    // factors so we go outside of the bounds a bit. Using these, we map alpha
+    // in [0,1] -> meaningful lamr.
+    const Scalar lamr_lo = 0.1*116.3, lamr_hi = 1.5*0.2e6;
     return (lamr_lo + alpha*(lamr_hi - lamr_lo))*(1 + mu_r);
-  };
+  }
 
-  const auto calc_mu_r = [&] (const Scalar& alpha) {
+  KOKKOS_FUNCTION static Scalar calc_mu_r (const Scalar& alpha) {
     return alpha*10;
-  };
+  }
 
   // Perform the table lookup and interpolation operations for (mu_r, lamr).
-  const auto interp = [&] (const Scalar& mu_r, const Scalar& lamr) {
+  KOKKOS_FUNCTION static Spack interp (const view_2d_table& table, const Scalar& mu_r,
+                                       const Scalar& lamr) {
     // Init the pack to all the same value, and compute in every pack slot.
     Smask qr_gt_small(true);
     Spack mu_r_p(mu_r), lamr_p(lamr);
     Table3 t3;
     Functions::lookup(qr_gt_small, mu_r_p, lamr_p, t3);
-    Spack val(qr_gt_small, Functions::apply_table(qr_gt_small, vm_table, t3));
+    Spack val(qr_gt_small, Functions::apply_table(qr_gt_small, table, t3));
     return val;
-  };
-
-  // Estimate two maximum slope magnitudes for two meshes, the second 10x
-  // refined w.r.t. the first.
-  Real slopes[2];
-  const Int nslopes = sizeof(slopes)/sizeof(*slopes);
-  Int N;
-
-  // Study the mu_r direction.
-  // For a sequence of refined meshes:
-  N = 1000;
-  for (Int refine = 0; refine < nslopes; ++refine) {
-    // Number of cells in the mesh.
-    N *= 10;
-    // Cell size relative to a parameter domain of 1.
-    const Scalar delta = 1.0/N;
-
-    // Interpolate at a specific (mu_r, lamr).
-    const Scalar lamr = calc_lamr(4.5, 0.5);
-    const auto eval = KOKKOS_LAMBDA (const Int& i) {
-      const auto alpha = double(i)/N;
-      const auto mu_r = calc_mu_r(alpha);
-      const auto val = interp(mu_r, lamr);
-      return std::log(val[0]);
-    };
-
-    // Compute the slope magnitude at a specific (mu_r, lamr) in the mu_r
-    // direction.
-    const auto get_max_slope = KOKKOS_LAMBDA (const Int& i, Scalar& slope) {
-      slope = util::max(slope, std::abs((eval(i+1) - eval(i))/delta));
-    };
-
-    Scalar max_slope;
-    Kokkos::parallel_reduce(RangePolicy(0, N), get_max_slope,
-                            Kokkos::Max<Scalar>(max_slope));
-    Kokkos::fence();
-    slopes[refine] = max_slope;
   }
 
-  // Now that we have collected slopes as a function of 10x mesh refinement,
-  // determine whether the slope estimates are converging. If they are, we can
-  // conclude there are no discontinuities. If they are not, then we are sensing
-  // a discontinuity, which is a bug.
-  //   In detail, for a 10x mesh refinement, a good slope growth rate is right
-  // around 1, and a bad one is is roughly 10. We set the threshold at 1.1.
-  const auto check_growth = [&] (const std::string& label, const Scalar& growth) {
-    if (growth > 1.1) {
-      std::cout << "Table3 FAIL: Slopes in the " << label << " direction are "
-      << slopes[0] << " and " << slopes[1]
-      << ", which grows by factor " << growth
-      << ". Near 1 is good; near 10 is bad.\n";
-      ++nerr;
+  static int run () {
+    Int nerr = 0;
+
+    // This test doesn't use mu_r_table, as that is not a table3 type. It
+    // doesn't matter whether we use vm_table or vn_table, as the table values
+    // don't matter in what we are testing; we are testing interpolation
+    // procedures that are indepennt of particular table values.
+    view_1d_table mu_r_table;
+    view_2d_table vn_table, vm_table;
+    Functions::init_kokkos_tables(vn_table, vm_table, mu_r_table);
+
+    // Estimate two maximum slope magnitudes for two meshes, the second 10x
+    // refined w.r.t. the first.
+    Real slopes[2];
+    const Int nslopes = sizeof(slopes)/sizeof(*slopes);
+    Int N;
+
+    // Study the mu_r direction.
+    // For a sequence of refined meshes:
+    N = 1000;
+    for (Int refine = 0; refine < nslopes; ++refine) {
+      // Number of cells in the mesh.
+      N *= 10;
+      // Cell size relative to a parameter domain of 1.
+      const Scalar delta = 1.0/N;
+
+      // Compute the slope magnitude at a specific (mu_r, lamr) in the mu_r
+      // direction.
+      const Scalar lamr = calc_lamr(4.5, 0.5);
+      const auto get_max_slope = KOKKOS_LAMBDA (const Int& i, Scalar& slope) {
+        // Interpolate at a specific (mu_r, lamr).
+        const auto eval = KOKKOS_LAMBDA (const Int& i) {
+          const auto alpha = double(i)/N;
+          const auto mu_r = calc_mu_r(alpha);
+          const auto val = interp(vm_table, mu_r, lamr);
+          return std::log(val[0]);
+        };
+        slope = util::max(slope, std::abs((eval(i+1) - eval(i))/delta));
+      };
+
+      Scalar max_slope;
+      Kokkos::parallel_reduce(RangePolicy(0, N), get_max_slope,
+                              Kokkos::Max<Scalar>(max_slope));
+      Kokkos::fence();
+      slopes[refine] = max_slope;
     }
-  };
-  check_growth("mu_r", slopes[1]/slopes[0]);
 
-  // Study the lamr direction.
-  N = 1000;
-  for (Int refine = 0; refine < nslopes; ++refine) {
-    N *= 10;
-    const Scalar delta = 1.0/N;
-
-    // Interpolate at a specific (mu_r, lamr).
-    const Scalar mu_r = 3.5;
-    const auto eval = KOKKOS_LAMBDA (const Int& i) {
-      const auto alpha = double(i)/N;
-      const auto lamr = calc_lamr(mu_r, alpha);
-      const auto val = interp(mu_r, lamr);
-      return std::log(val[0]);
+    // Now that we have collected slopes as a function of 10x mesh refinement,
+    // determine whether the slope estimates are converging. If they are, we can
+    // conclude there are no discontinuities. If they are not, then we are sensing
+    // a discontinuity, which is a bug.
+    //   In detail, for a 10x mesh refinement, a good slope growth rate is right
+    // around 1, and a bad one is is roughly 10. We set the threshold at 1.1.
+    const auto check_growth = [&] (const std::string& label, const Scalar& growth) {
+      if (growth > 1.1) {
+        std::cout << "Table3 FAIL: Slopes in the " << label << " direction are "
+        << slopes[0] << " and " << slopes[1]
+        << ", which grows by factor " << growth
+        << ". Near 1 is good; near 10 is bad.\n";
+        ++nerr;
+      }
     };
+    check_growth("mu_r", slopes[1]/slopes[0]);
 
-    // Compute the slope magnitude at a specific (mu_r, lamr) in the lamr
-    // direction.
-    const auto get_max_slope = KOKKOS_LAMBDA (const Int& i, Scalar& slope) {
-      slope = util::max(slope, std::abs((eval(i+1) - eval(i))/delta));
-    };
+    // Study the lamr direction.
+    N = 1000;
+    for (Int refine = 0; refine < nslopes; ++refine) {
+      N *= 10;
+      const Scalar delta = 1.0/N;
 
-    Scalar max_slope;
-    Kokkos::parallel_reduce(RangePolicy(0, N), get_max_slope,
-                            Kokkos::Max<Scalar>(max_slope));
-    Kokkos::fence();
-    slopes[refine] = max_slope;
-  }
-  check_growth("lamr", slopes[1]/slopes[0]);
+      // Compute the slope magnitude at a specific (mu_r, lamr) in the lamr
+      // direction.
+      const Scalar mu_r = 3.5;
+      const auto get_max_slope = KOKKOS_LAMBDA (const Int& i, Scalar& slope) {
+        // Interpolate at a specific (mu_r, lamr).
+        const auto eval = KOKKOS_LAMBDA (const Int& i) {
+          const auto alpha = double(i)/N;
+          const auto lamr = calc_lamr(mu_r, alpha);
+          const auto val = interp(vm_table, mu_r, lamr);
+          return std::log(val[0]);
+        };
+        slope = util::max(slope, std::abs((eval(i+1) - eval(i))/delta));
+      };
+
+      Scalar max_slope;
+      Kokkos::parallel_reduce(RangePolicy(0, N), get_max_slope,
+                              Kokkos::Max<Scalar>(max_slope));
+      Kokkos::fence();
+      slopes[refine] = max_slope;
+    }
+    check_growth("lamr", slopes[1]/slopes[0]);
   
-  return nerr;
-}
+    return nerr;
+  }
+};
 
-// r[1] is a mixing ratio field. The test advects r[1] some number of time
-// steps. The test checks global mass conservation and extrema non-violation at
+  // r[1] is a mixing ratio field. The test advects r[1] some number of time
+  // steps. The test checks global mass conservation and extrema non-violation at
 // each step. B/c of the consistency issue noted in the
 // calc_first_order_upwind_step doc, r[0] = 1 initially so that r[0]*rho is the
 // true, i.e. correctly advected, total density. Note that r[0] will not remain
@@ -918,7 +920,7 @@ int main (int argc, char** argv) {
 
       if (brief && nt == upper) {
         wrap_test("workspace", UnitTest<>::unittest_workspace());
-        wrap_test("table3", UnitTest<>::unittest_table3(nt));
+        wrap_test("table3", UnitTest<>::TestTable3::run());
       }
       
       // thread-sensitive tests
