a96456a70eb5fe3a986b4e5ac0ae773710f8fa87
Peter Caldwell <caldwell19@llnl.gov>
2020-10-29 16:09:52 -0700

    clean up run script

--- run_scripts/run.ne256pg2_scream.dy2.1024nodes.sh
+++ run_scripts/run.ne256pg2_scream.dy2.1024nodes.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Who to send email updates on run status to
-email_address="caldwell19@llnl.gov"
-
 # Set run options
+#=============================================
 resolution=ne256pg2_r0125_oRRS18to6v3
 compset=F2010-SCREAM-HR-DYAMOND2
 branch=autoconv_25perc
@@ -13,7 +11,7 @@ compiler=intel
 stop_option="ndays"
 stop_n="1"
 rest_n="1"
-walltime="8:29:00"
+walltime="2:00:00"
 queue="regular"
 
 # Setup processor layout
@@ -31,6 +29,9 @@ else
 fi
 pelayout=${nnodes}x${mpi_tasks_per_node}x${nthreads}
 
+# Who to send email updates on run status to
+email_address="caldwell19@llnl.gov"
+
 # Set flags specific to running this script
 do_download=false
 do_newcase=true
@@ -41,14 +42,17 @@ do_submit=true
 debug_compile='FALSE'
 
 # Set paths
-# Set paths
 datestring=`date +"%Y%m%d-%H"`
 case_name=${branch}.${resolution}.${compset}.${machine}_${compiler}.${pelayout}.DY2_Oct6.${datestring}
-#case_name=${branch}.testbuild.${datestring}
 code_root=${HOME}/gitwork/scream/
 case_root=${CSCRATCH}/E3SM_runs/${case_name}
 
+######################################################################################
+### USERS PROBABLY DON'T NEED TO CHANGE ANYTHING BELOW HERE EXCEPT user_nl_* FILES ###
+######################################################################################
+
 # Download code
+#=============================================
 if [ "${do_download}" == "true" ]; then
 
     echo "Cloning repository repo = $repo into branch = $branch under code_root = $code_root"
@@ -65,7 +69,7 @@ if [ "${do_download}" == "true" ]; then
     git config commit.template $code_root/.git/hooks/commit.template
 
     # Bring in all submodule components
-    git submodule update --init
+    git submodule update --init --recursive
 
     # Check out desired branch
     git checkout ${branch}
@@ -74,6 +78,7 @@ if [ "${do_download}" == "true" ]; then
 fi
 
 # Create case
+#=============================================
 if [ "${do_newcase}" == "true" ]; then
     ${code_root}/cime/scripts/create_newcase \
         --case ${case_root} \
@@ -87,12 +92,12 @@ fi
 cp -v `basename $0` ${case_root}/
 
 # Setup
+#=============================================
 if [ "${do_setup}" == "true" ]; then
     cd ${case_root}
 
     # Set run length
     ./xmlchange STOP_OPTION=${stop_option},STOP_N=${stop_n}
-
     ./xmlchange REST_N=${rest_n}
 
     # Set processor layout
@@ -107,26 +112,22 @@ if [ "${do_setup}" == "true" ]; then
     ./xmlchange MAX_MPITASKS_PER_NODE=${mpi_tasks_per_node}
     ./xmlchange MAX_TASKS_PER_NODE=${total_tasks_per_node}
 
-
     # Flag for debug compile
     ./xmlchange --id DEBUG --val ${debug_compile}
-    # # Set PIO format, use PIO version 2, and increase PIO buffer size 
+    
+    # Set PIO format, use PIO version 2, and increase PIO buffer size 
     ./xmlchange PIO_NETCDF_FORMAT="64bit_data"
     ./xmlchange PIO_VERSION="2"
-    #./xmlchange PIO_BUFFER_SIZE_LIMIT=134217728
     ./xmlchange PIO_BUFFER_SIZE_LIMIT=64200000
     
-
-
-    
     # Edit CAM namelist to set dycore options for new grid
-    cat <<EOF >> user_nl_cam
+    cat <<EOF >> user_nl_eam
 
     ! Don't write h0 files
     empty_htapes=.true.
     ! Outputs for DYAMOND
-    nhtfrq = 0, 3,3,-3 !,-1
-    mfilt = 1, 12,12,1 !,12
+    nhtfrq = 0, 3,3,-3 !output freq:   monthly, 15 min, 15 min, 3 hrly
+    mfilt = 1, 96,96,8 !new file freq: monthly, daily, daily, daily 
     fincl2 = 'U10', 'UBOT:I', 'VBOT:I', 'TREFHT', 'PS', 'QREFHT', 'CLDLOW', 'CLDMED', 'CLDHGH', 
              'TMQ', 'TMCLDLIQ', 'TMCLDICE', 'TMRAINQM', 'TMCLDRIM', 'CLDTOT', 'SHFLX', 'TGCLDLWP', 
              'LHFLX', 'TAUX', 'TAUY', 'PRECT','PRECSL', 'QFLX', 'FSNS',  'FSNTOA',  'FSDS',  'FLNS', 'FLNT', 
@@ -138,9 +139,9 @@ if [ "${do_setup}" == "true" ]; then
              'TBOT:M', 'TS:M','U850','Q700','Q500','OMEGA200','OMEGA700','Z850',
              'Q700', 'Q500', 'OMEGA200', 'OMEGA700', 'Z850'
     fincl4 = 'U:I', 'V:I', 'WLARGE:I', 'T:I', 'PS:I', 'Q:I', 'CLDLIQ:I', 'CLDICE:I','NUMICE:I','NUMLIQ:I'
-    ! Do radiation every five minutes
-    !iradsw = 4
-    !iradlw = 4
+    ! Radiation must be called every output timestep
+    iradsw = 3
+    iradlw = 3
 
 EOF
 
@@ -152,7 +153,8 @@ EOF
     # Finally, run setup
     ./case.setup
 
-    # Link to run directory
+    # The run location is determined in the bowels of CIME
+    # Symlink to that location from user-chosen $case_root (=current dir)
     ln -s `./xmlquery -value RUNDIR` run
 
 
@@ -160,10 +162,10 @@ EOF
     ./xmlchange --file env_run.xml TPROF_TOTAL=-1
     echo "tprof_n = 1" >> user_nl_cpl
     echo "tprof_option = 'nsteps'" >> user_nl_cpl
-
 fi
 
 # Build
+#=============================================
 if [ "${do_build}" == "true" ]; then
     cd ${case_root}
     ./case.build
@@ -172,10 +174,10 @@ if [ "${do_build}" == "true" ]; then
     ls -l run
     lfs setstripe -S 1m -c 64 run
     lfs getstripe run >& lfs_run.txt
-
 fi
 
 # Run
+#=============================================
 if [ "${do_submit}" == "true" ]; then
     cd ${case_root}
     ./case.submit --batch-args="--mail-type=ALL --mail-user=${email_address}"
