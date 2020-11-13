2d0b6c36dfde16d8b6716dd79a6f72f75d06eff5
crterai <rterai@uci.edu>
2020-11-13 09:28:47 -0800

    Changes to moving to storage script

---
+++ post-run_scripts/moving_ne1024_files_to_storage.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#$ -cwd
+
+# Script moves data to HPSS and verifies the integrity of the files
+
+# ============================================================
+
+# Location of the model output
+# USER needs to change this
+fileloc='/global/cscratch1/sd/terai/e3sm_scratch/cori-knl/20201112.SCREAMv0dyamond2.F2010-SCREAM-HR-DYAMOND2.ne1024pg2_r0125_oRRS18to6v3.cori-knl.1536x8x16/run/'
+
+# Location of the regridded files
+# USER needs to change this
+hpssloc='/home/t/terai/Production_runs/20201112.SCREAMv0dyamond2.F2010-SCREAM-HR-DYAMOND2.ne1024pg2_r0125_oRRS18to6v3.cori-knl.1536x8x16/'
+
+# =============================================================
+
+
+echo "  "
+echo " ---------------- Movement of data  -------------------- "
+echo "  "
+
+cd ${fileloc}
+
+
+for f in *.eam.h*.nc ; do
+    
+    echo "moving $f"
+    time hsi "cd ${hpssloc}; put -c on $f"
+done
+
+for f in *.eam.h*.nc ; do
+
+    echo "verifying $f"
+    hsi hashverify ${hpssloc} 2&>1 | tee move_verify.txt
+done
+
+
+echo "  "
+echo "  "
+
+echo "  "
+echo "  "
+echo "  "
+echo "   DONE!  "
+echo "  "
+echo "  "
+echo "  ******** "  
