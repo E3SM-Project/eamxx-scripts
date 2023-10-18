ddec2887a4befe66ecae83249d6cb2ddad943a3e
Benjamin Hillman <bhillma@sandia.gov>
2023-10-18 07:13:05 -0700

    Remove topo stuff from IC

--- preprocessing_scripts/convert_cami_to_screami.py
+++ preprocessing_scripts/convert_cami_to_screami.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-import xarray, numpy, dask
+import xarray, numpy, dask, sys
 from subprocess import call
 
-def main(inputfile, topofile, gasfile, outputfile):
+def print_message(message):
+    print(message); sys.stdout.flush()
+
+
+def main(inputfile, gasfile, outputfile):
 
     # Stuff we need to get from cami file
     screami_from_cami_coordvars = {  # These we want to be doubles
@@ -43,39 +47,27 @@ def main(inputfile, topofile, gasfile, outputfile):
         nlev = ds_in.sizes['lev']
 
         # Fields we can copy directly
-        print('Copy fields from cami')
+        print_message('Copy fields from cami')
         for scream_name, eam_name in screami_from_cami.items():
-            print(f'  {eam_name} -> {scream_name}')
+            print_message(f'  {eam_name} -> {scream_name}')
             if eam_name in ds_in.variables.keys():
                 ds_out[scream_name] = ds_in[eam_name]
             else:
-                print(f'WARNING: {eam_name} not found in inputfile; hope you did not need that one...')
+                print_message(f'WARNING: {eam_name} not found in inputfile; hope you did not need that one...')
 
         # Make sure ps field contains a time dimension
         if 'time' not in ds_out['ps'].dims:
             ds_out['ps'] = ds_out['ps'].expand_dims('time', axis=0)
 
         # Handle U,V to horiz_winds(time,ncol,dim2,lev)
-        print('Map U,V to horiz_winds')
+        print_message('Map U,V to horiz_winds')
         ds_out['horiz_winds'] = xarray.concat([ds_in['U'], ds_in['V']], 'dim2')
 
-        # Grab phis from topo file
-        print('Grab phis from topo file')
-        with xarray.open_dataset(topofile, decode_cf=False) as ds_topo:
-            if 'PHIS_d' in ds_topo.variables.keys():
-                print('  use PHIS_d')
-                ds_out['phis'] = ds_topo['PHIS_d'].rename({'ncol_d': 'ncol'})
-            else:
-                print('  use PHIS')
-                ds_out['phis'] = ds_topo['PHIS']
-        # Broadcast phis to include time dim
-        ds_out['phis'], *__ = xarray.broadcast(ds_out['phis'], ds_out['ps'])
-
         # Grab trace gas concentrations from file
-        print('Get trace gases')
+        print_message('Get trace gases')
         with xarray.open_dataset(gasfile, decode_cf=False).isel(time=0).drop('time') as ds_gas:
             for v in gas_names:
-                print(f'  get {v} from gas file...')
+                print_message(f'  get {v} from gas file...')
                 ds_out[v + '_volume_mix_ratio'] = xarray.DataArray(
                     numpy.zeros([ncol, nlev], dtype=float), 
                     dims=('ncol', 'lev'),
@@ -89,18 +81,18 @@ def main(inputfile, topofile, gasfile, outputfile):
         # Convert datavars to single precision float
         for v in ds_out.data_vars.keys():
             if v not in screami_from_cami_coordvars.keys():
-                print(f'convert {v} to float...')
+                print_message(f'convert {v} to float...')
                 ds_out[v] = ds_out[v].astype(numpy.float32)
 
         # Permute dimensions
-        print('Permute dimensions')
+        print_message('Permute dimensions')
         try:
             if 'nbnd' in ds_out.dims:
                 ds_out = ds_out.transpose('time','ncol','dim2','lev','ilev','nbnd')
             else:
                 ds_out = ds_out.transpose('time','ncol','dim2','lev','ilev')
         except:
-            print('  permute dimensions failed, but continuing anyways.')
+            print_message('  permute dimensions failed, but continuing anyways.')
 
         # Apply clipping
         for v in ('qv', 'qc', 'qi', 'qr', 'nc', 'ni', 'nr'):
@@ -110,17 +102,17 @@ def main(inputfile, topofile, gasfile, outputfile):
         # Check values
         for v in ds_out.variables:
             numnan = ds_out[v].isnull().sum().values
-            print(f'{v} numnan = {numnan}')
+            print_message(f'{v} numnan = {numnan}')
 
-        print('Write to netcdf')
+        print_message('Write to netcdf')
         ds_out.to_netcdf(outputfile) #format="NETCDF3_64BIT")
 
         # Convert to cdf5
-        print('Convert to cdf5')
+        print_message('Convert to cdf5')
         call(f'ncks -5 {outputfile} {outputfile}.cdf5'.split(' '))
         call(f'mv {outputfile}.cdf5 {outputfile}'.split(' '))
 
-        print('Done.')
+        print_message('Done.')
 
 if __name__ == '__main__':
     import plac; plac.call(main)
