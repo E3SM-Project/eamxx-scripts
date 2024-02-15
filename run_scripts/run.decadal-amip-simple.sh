f15ec5e555d927491aa1bda31b1e138917729e61
Benjamin Hillman <bhillma@sandia.gov>
2024-02-15 00:17:38 -0500

    Edits to runscript

--- run_scripts/run.decadal-amip-simple.sh
+++ run_scripts/run.decadal-amip-simple.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
+umask 022
 
 branch="add-F20TR-SCREAMv1"
 code_root=${HOME}/codes/scream/branches/${branch}
-res=ne256pg2_ne256pg2 #ne1024pg2_ne1024pg2 #ne30pg2_EC30to60E2r2
+screamdocs_root=${HOME}/codes/scream-docs/branches/add-decadal-runscript
+res=ne1024pg2_ne1024pg2 #ne1024pg2_ne1024pg2 #ne30pg2_EC30to60E2r2
 compset=F20TR-SCREAMv1
 machine="frontier-scream-gpu" #chrysalis
 compiler="crayclang-scream" #intel
 project="cli115"
-casename=${branch}.${res}.${compset}.${machine}_${compiler}.debugMOSART
+walltime="01:00:00"
+casename=${branch}.${res}.${compset}.${machine}_${compiler}.newO3
 caseroot=${HOME}/codes/scream/cases/${casename}
 #readonly pecount="1536x6" # 192 nodes
 #readonly pecount="3072x6" # 384 nodes
@@ -16,7 +19,7 @@ caseroot=${HOME}/codes/scream/cases/${casename}
 #readonly pecount="8192x6" # 1024 nodes
 #readonly pecount="15056x6" # 1882 nodes
 if [ "${res}" == "ne1024pg2_ne1024pg2" ]; then
-    pecount="16384x6" # 2048 nodes
+    pecount="2560x6" # 320 nodes "16384x6" # 2048 nodes
 elif [ "${res}" == "ne256pg2_ne256pg2" ]; then
     pecount="768x6" # 96 nodes
 elif [ "${res}" == "ne30pg2_EC30to60E2r2" ]; then
@@ -32,7 +35,7 @@ ${code_root}/cime/scripts/create_newcase \
     --compiler=${compiler} \
     --pecount=${pecount} \
     --project=${project} \
-    --walltime=01:00:00
+    --walltime=${walltime}
 
 cd ${caseroot}
 
@@ -53,7 +56,8 @@ if [ "${machine}" == "frontier-scream-gpu" ]; then
 fi
 
 # Change run length
-./xmlchange STOP_OPTION=nhours,STOP_N=1
+./xmlchange STOP_OPTION=ndays,STOP_N=1
+./xmlchange REST_OPTION=never  # BRHDEBUG!
 ./xmlchange RESUBMIT=0
 ./xmlchange RUN_STARTDATE="1994-10-01"
 
@@ -71,15 +75,27 @@ fi
 
 # Namelist options for EAMxx
 if [ "${res}" == "ne1024pg2_ne1024pg2" ]; then
-    ./atmchange initial_conditions::Filename="\${DIN_LOC_ROOT}/atm/scream/init/screami_ne1024np4L128_era5-19941001-topoadjx6t_20240123.nc"
+    ./atmchange initial_conditions::Filename="${DIN_LOC_ROOT}/atm/scream/init/screami_ne1024np4L128_era5-19941001-topoadjx6t_20240214.nc"
 elif [ "${res}" == "ne256pg2_ne256pg2" ]; then
-    ./atmchange initial_conditions::Filename="\${DIN_LOC_ROOT}/atm/scream/init/screami_ne256np4L128_era5-19941001-topoadjx6t_20240123.nc"
+    ./atmchange initial_conditions::Filename="${DIN_LOC_ROOT}/atm/scream/init/screami_ne256np4L128_era5-19941001-topoadjx6t_20240123.nc"
 fi
 
 # Turn on cosp and set frequency
 ./atmchange physics::atm_procs_list="mac_aero_mic,rrtmgp,cosp"
 ./atmchange physics::cosp::cosp_frequency_units="hours"
 ./atmchange physics::cosp::cosp_frequency=1
+
+# Need to explicitly turn on computing tendencies
+./atmchange physics::mac_aero_mic::shoc::compute_tendencies=T_mid,qv
+./atmchange physics::mac_aero_mic::p3::compute_tendencies=T_mid,qv
+./atmchange physics::rrtmgp::compute_tendencies=T_mid
+./atmchange homme::compute_tendencies=T_mid,qv
+
+# Set temperature cut off in dycore threshold to 180K
+./atmchange vtheta_thresh=180
+
+# Change lambda_high
+./atmchange lambda_high=0.08
 
 # use GHG levels more appropriate for sim
 # Average from 19940101 - 20150101
@@ -92,59 +108,41 @@ fi
 # write out DAG
 ./atmchange atmosphere_dag_verbosity_level=5
 
-if [ 1 -eq 0 ]; then  # BRHDEBUG
 # Copy output stream yaml files
-cp ${SCREAMDOCS_ROOT}"/v1_output/scream_output.Cess.monthly_ne1024.yaml" .
-cp ${SCREAMDOCS_ROOT}"/v1_output/scream_output.Cess.50hourly_QcQiNcNi.yaml" .
-cp ${SCREAMDOCS_ROOT}"/v1_output/scream_output.Cess.50hourly_QrNrQmBm.yaml" .
-cp ${SCREAMDOCS_ROOT}"/v1_output/scream_output.Cess.6hourlyINST_ne30.yaml" .
-cp ${SCREAMDOCS_ROOT}"/v1_output/scream_output.Cess.6hourlyAVG_ne30.yaml" .
-cp ${SCREAMDOCS_ROOT}"/v1_output/scream_output.Cess.3hourlyAVG_ne120.yaml" .
-cp ${SCREAMDOCS_ROOT}"/v1_output/scream_output.Cess.3hourlyINST_ne120.yaml" .
-cp ${SCREAMDOCS_ROOT}"/v1_output/scream_output.Cess.3hourly_ne1024.yaml" .
-cp ${SCREAMDOCS_ROOT}"/v1_output/scream_output.Cess.hourly_2Dvars.yaml" .
-cp ${SCREAMDOCS_ROOT}"/v1_output/scream_output.Cess.ARM_sites_2D.yaml" .
-cp ${SCREAMDOCS_ROOT}"/v1_output/scream_output.Cess.ARM_sites_3D.yaml" .
-cp ${SCREAMDOCS_ROOT}"/v1_output/scream_output.Cess.monthly_cosp_ne1024.yaml" .
-cp ${SCREAMDOCS_ROOT}"/v1_output/scream_output.Cess.ACI_regions_2D.yaml" .
-# Set output streams
-./atmchange output_yaml_files="./scream_output.Cess.monthly_ne1024.yaml"
-./atmchange output_yaml_files+="./scream_output.Cess.50hourly_QcQiNcNi.yaml"
-./atmchange output_yaml_files+="./scream_output.Cess.50hourly_QrNrQmBm.yaml"
-./atmchange output_yaml_files+="./scream_output.Cess.6hourlyINST_ne30.yaml"
-./atmchange output_yaml_files+="./scream_output.Cess.6hourlyAVG_ne30.yaml"
-./atmchange output_yaml_files+="./scream_output.Cess.3hourlyAVG_ne120.yaml"
-./atmchange output_yaml_files+="./scream_output.Cess.3hourlyINST_ne120.yaml"
-./atmchange output_yaml_files+="./scream_output.Cess.3hourly_ne1024.yaml"
-./atmchange output_yaml_files+="./scream_output.Cess.hourly_2Dvars.yaml"
-./atmchange output_yaml_files+="./scream_output.Cess.ARM_sites_2D.yaml"
-./atmchange output_yaml_files+="./scream_output.Cess.ARM_sites_3D.yaml"
-./atmchange output_yaml_files+="./scream_output.Cess.monthly_cosp_ne1024.yaml"
-./atmchange output_yaml_files+="./scream_output.Cess.ACI_regions_2D.yaml"
-fi
-
+output_yaml_files=(`ls ${screamdocs_root}/v1_output/decadal/*.yaml`)
+for file in ${output_yaml_files[@]}; do
+    # TODO: add remap file replacement for different grids
+    cp -v ${file} ./
+    if [ "${file}" == "${output_yaml_files[0]}" ]; then
+        # First file, reset output list
+        ./atmchange output_yaml_files="./`basename ${file}`"
+    else
+        # Append to output list
+        ./atmchange output_yaml_files+="./`basename ${file}`"
+    fi
+done
 
 # Namelist options for ELM
 # Specify land initial condition and surface datasets
 if [ "${res}" == "ne1024pg2_ne1024pg2" ]; then
 cat << EOF >> user_nl_elm
   ! Set input data paths
-  finidat = "\${DIN_LOC_ROOT}/lnd/clm2/initdata/20231226.I2010CRUELM.ne1024pg2_ICOS10.elm.r.1994-10-01-00000.nc"
-  flanduse_timeseries = "\${DIN_LOC_ROOT}/lnd/clm2/surfdata_map/landuse.timeseries_ne1024pg2_historical_simyr1990-2014_c240109.nc"
-  fsurdat = "\${DIN_LOC_ROOT}/lnd/clm2/surfdata_map/surfdata_ne1024pg2_simyr2010_c211021.nc"
+  finidat = "${DIN_LOC_ROOT}/lnd/clm2/initdata/20231226.I2010CRUELM.ne1024pg2_ICOS10.elm.r.1994-10-01-00000.nc"
+  flanduse_timeseries = "${DIN_LOC_ROOT}/lnd/clm2/surfdata_map/landuse.timeseries_ne1024pg2_historical_simyr1990-2014_c240109.nc"
+  fsurdat = "${DIN_LOC_ROOT}/lnd/clm2/surfdata_map/surfdata_ne1024pg2_simyr2010_c211021.nc"
 EOF
 elif [ "${res}" == "ne256pg2_ne256pg2" ]; then
 cat << EOF >> user_nl_elm
   ! Set input data paths
-  finidat = "\${DIN_LOC_ROOT}/lnd/clm2/initdata/20240104.I2010CRUELM.ne256pg2.elm.r.1994-10-01-00000.nc"
-  flanduse_timeseries = "\${DIN_LOC_ROOT}/lnd/clm2/surfdata_map/landuse.timeseries_ne256pg2_hist_simyr1850-2015_c240131.nc"
-  fsurdat = "\${DIN_LOC_ROOT}/lnd/clm2/surfdata_map/surfdata_ne256pg2_simyr1850_c240131.nc"
+  finidat = "${DIN_LOC_ROOT}/lnd/clm2/initdata/20240104.I2010CRUELM.ne256pg2.elm.r.1994-10-01-00000.nc"
+  flanduse_timeseries = "${DIN_LOC_ROOT}/lnd/clm2/surfdata_map/landuse.timeseries_ne256pg2_hist_simyr1850-2015_c240131.nc"
+  fsurdat = "${DIN_LOC_ROOT}/lnd/clm2/surfdata_map/surfdata_ne256pg2_simyr1850_c240131.nc"
 EOF
 elif [ "${res}" == "ne30pg2_EC30to60E2r2" ]; then
 cat << EOF >> user_nl_elm
-  finidat = "\$DIN_LOC_ROOT/lnd/clm2/initdata/20210802.ICRUELM-1950.ne30pg2_EC30to60E2r2.elm.r.0051-01-01-00000.nc"
-  flanduse_timeseries = "\${DIN_LOC_ROOT}/lnd/clm2/surfdata_map/landuse.timeseries_ne30np4.pg2_hist_simyr1850-2015_c210113.nc"
-  fsurdat = "\${DIN_LOC_ROOT}/lnd/clm2/surfdata_map/surfdata_ne30pg2_simyr1850_c230417_with_TOP.nc"
+  finidat = "$DIN_LOC_ROOT/lnd/clm2/initdata/20210802.ICRUELM-1950.ne30pg2_EC30to60E2r2.elm.r.0051-01-01-00000.nc"
+  flanduse_timeseries = "${DIN_LOC_ROOT}/lnd/clm2/surfdata_map/landuse.timeseries_ne30np4.pg2_hist_simyr1850-2015_c210113.nc"
+  fsurdat = "${DIN_LOC_ROOT}/lnd/clm2/surfdata_map/surfdata_ne30pg2_simyr1850_c230417_with_TOP.nc"
 EOF
 fi
 
@@ -171,9 +169,15 @@ cat << EOF >> user_nl_elm
 EOF
 
 # Set MOSART initial condition
+if [ "${res}" == "ne1024pg2_ne1024pg2" ]; then
 cat << EOF >> user_nl_mosart
-  finidat_rtm = "\${DIN_LOC_ROOT}/rof/mosart/initdata/20231226.I2010CRUELM.ne1024pg2_ICOS10.mosart.r.1994-10-01-00000.nc"
+  finidat_rtm = "${DIN_LOC_ROOT}/rof/mosart/initdata/20231226.I2010CRUELM.ne1024pg2_ICOS10.mosart.r.1994-10-01-00000.nc"
 EOF
+elif [ "${res}" == "ne256pg2_ne256pg2" ]; then
+cat << EOF >> user_nl_mosart
+  finidat_rtm = "${DIN_LOC_ROOT}/rof/mosart/initdata/20240104.I2010CRUELM.ne256pg2.mosart.r.1994-10-01-00000.nc"
+EOF
+fi
 
 # Coupler settings; new surface flux scheme
 cat << EOF >> user_nl_cpl
@@ -181,8 +185,8 @@ cat << EOF >> user_nl_cpl
 EOF
 
 # Point to new SST forcing
-./xmlchange --file env_run.xml --id SSTICE_DATA_FILENAME --val "\${DIN_LOC_ROOT}/atm/cam/sst/sst_ostia_3600x7200_19940930_20151231_c20240125.nc"
-./xmlchange --file env_run.xml --id SSTICE_GRID_FILENAME --val "\${DIN_LOC_ROOT}/ocn/docn7/domain.ocn.3600x7200.230522.nc"
+./xmlchange --file env_run.xml --id SSTICE_DATA_FILENAME --val "${DIN_LOC_ROOT}/atm/cam/sst/sst_ostia_3600x7200_19940930_20151231_c20240125.nc"
+./xmlchange --file env_run.xml --id SSTICE_GRID_FILENAME --val "${DIN_LOC_ROOT}/ocn/docn7/domain.ocn.3600x7200.230522.nc"
 ./xmlchange --file env_run.xml --id SSTICE_YEAR_ALIGN --val 1994
 ./xmlchange --file env_run.xml --id SSTICE_YEAR_START --val 1994
 ./xmlchange --file env_run.xml --id SSTICE_YEAR_END --val 2015
@@ -190,5 +194,5 @@ EOF
 # Setup, build, run
 ./case.setup
 ./case.build
-#./case.submit
+./case.submit -a="--job-name=${casename} -t ${walltime} --mail-type=END --mail-user=bhillma@sandia.gov"
 echo "${caseroot}"
