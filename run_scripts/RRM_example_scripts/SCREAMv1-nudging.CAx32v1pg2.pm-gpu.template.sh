a4e9350abbc0e3453592e3b61740921c8ec0d0d9
Gautam Bisht <gautam.bisht@pnnl.gov>
2025-07-21 15:27:06 -0700

    Changes addition of EAMxx output files to comment

--- run_scripts/RRM_example_scripts/SCREAMv1-nudging.CAx32v1pg2.pm-gpu.template.sh
+++ run_scripts/RRM_example_scripts/SCREAMv1-nudging.CAx32v1pg2.pm-gpu.template.sh
@@ -40,11 +40,9 @@ readonly Q=regular
 readonly COMPSET="F2010-SCREAMv1"
 readonly RESOLUTION="CAx32v1pg2_CAx32v1pg2"
 
-# Directory to where your YAML (output) files are located. Do a search for "YAML_ROOT"
-#   to find the location in this script where you will specify the individual files.
-readonly YAML_ROOT="/global/homes/b/bogensch/scream_v1_scripts/yaml_output_files"
+# Define the path to your sample EAMxx output yaml files
+# readonly YAML_ROOT="<path-to-your-output-yaml-fies>"
 
-# Directory where your nudging data is located
 readonly NUDGING_ROOT="/pscratch/sd/z/zhang73/DATA/data_hiccup/L128.mono_CAx32v1pg2.UVTQ.pres.cdsnew.240929/"
 
 # Directory where your code is
@@ -448,8 +446,13 @@ runtime_options() {
 	exit 380
     fi
 
-    cp ${YAML_ROOT}"/scream_output.monthly.yaml" .
-    ./atmchange output_yaml_files="./scream_output.monthly.yaml"
+   # Copy the EAMxx output yaml files and add them via ./atmchange
+   #
+   # cp ${YAML_ROOT}"/output-1.yaml" .
+   # cp ${YAML_ROOT}"/output-2.yaml" .
+   #
+   # ./atmchange output_yaml_files="./output-1.yaml"
+   # ./atmchange output_yaml_files+="./output-2.yaml"
 
     popd
 }
